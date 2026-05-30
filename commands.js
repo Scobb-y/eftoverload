38eb2330f1eeb099040f8e49c96f1901a7333b4e
Scobb-y <ben.kyd10@gmail.com>
2026-05-30 13:29:25 -0400

    Initial commit

---
+++ commands.js
@@ -0,0 +1,4 @@
+module.exports = {
+  name: 'ammoChart',
+  description: 'Display Ammo Chart'
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { ButtonBuilder, ButtonStyle, Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.DirectMessages
+  ],
+});
+
+const btn = new ButtonBuilder()
+    .setCustomId('ammoChart')
+    .setLabel('Ammo Chart')
+    .setStyle(ButtonStyle.Primary)
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on("")
